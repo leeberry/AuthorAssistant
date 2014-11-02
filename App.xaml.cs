09b9464857114a7f6f91d94b70082ddb44accd79
leeberry <lee.berry@vanilla99.com>
2014-11-02 22:10:54 +0000

    1.0

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace AuthorsAssistant
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ ViewModel/DelegateCommand.cs
@@ -0,0 +1,61 @@
+﻿namespace AuthorsAssistant.ViewModel
+{
+    using System;
+    using System.Windows.Input;
+
+    /// <summary>
+    /// Delegate to which the commands in the view can bind
+    /// </summary>
+    public class DelegateCommand : ICommand
+    {
+        /// <summary>
+        /// The action.
+        /// </summary>
+        private readonly Action action;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DelegateCommand"/> class
+        /// </summary>
+        /// <param name="action">
+        /// The action.
+        /// </param>
+        public DelegateCommand(Action action)
+        {
+            this.action = action;
+        }
+
+#pragma warning disable 67
+        /// <summary>
+        /// The can execute changed.
+        /// </summary>
+        public event EventHandler CanExecuteChanged;
+#pragma warning restore 67
+
+        /// <summary>
+        /// The execute.
+        /// </summary>
+        /// <param name="parameter">
+        /// The parameter.
+        /// </param>
+        public void Execute(object parameter)
+        {
+            this.action();
+        }
+
+        /// <summary>
+        /// The can execute.
+        /// </summary>
+        /// <param name="parameter">
+        /// The parameter.
+        /// </param>
+        /// <returns>
+        /// The <see cref="bool"/>.
+        /// </returns>
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+    }
+}
+
+
